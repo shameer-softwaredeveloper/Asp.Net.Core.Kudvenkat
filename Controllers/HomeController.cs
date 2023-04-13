9c5445b92e5aa61851d3b73732e6a9380e887892
Your Name <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-04-13 07:20:59 +0530

    #16 Hello from mvc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+namespace EmployeeManagement
+{
+    public class HomeController
+    {
+        // http://localhost:5000/
+        // http://localhost:5000/home/index
+        public string Index()
+        {
+            return "Hello from MVC";
+        }
+    }
+}
