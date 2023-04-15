0274994801c60529a2942c431a8715589ef08784
Your Name <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-04-15 15:45:48 +0530

    #23 ViewData Weakly typed Dictionary, as keyword to resolve the type at runtime, @{ }, @

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,6 +19,8 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
+            ViewData["Employee"] = model;
+            ViewData["PageTitle"] = "Employee Details";
             return View(); 
         }
     }
