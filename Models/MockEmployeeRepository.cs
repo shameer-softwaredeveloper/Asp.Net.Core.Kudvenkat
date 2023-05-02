2b7acd5d3034b51e32f8d1576b9caa6197be44bd
you@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-02 22:14:12 +0530

    #49 In Memory Repository

--- Models/MockEmployeeRepository.cs
+++ Models/MockEmployeeRepository.cs
@@ -33,5 +33,27 @@ namespace EmployeeManagement.Models
             _employeeList.Add(employee);
             return employee;
         }
+
+        Employee Update(Employee employeeChanges)
+        {
+            Employee employee = _employeeList.FirstOrDefault(e => e.Id == employeeChanges.Id);
+            if(employee != null)
+            {
+                employee.Name = employeeChanges.Name;
+                employee.Email = employeeChanges.Email;
+                employee.Department = employeeChanges.Department;
+            }
+            return employee; 
+        }
+
+        Employee Delete(int id)
+        {
+            Employee employee = _employeeList.FirstOrDefault(e => e.Id == id);
+            if(employee != null)
+            {
+                _employeeList.Remove(employee);
+            }
+            return employee; 
+        }
     }
 }
