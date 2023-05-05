cb344c1bc00152d92795db45928d8fb71a73a56c
you@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-05 22:18:57 +0530

    #51 EF seed data modelBuilder.Entity<Employee>().HasData(new Employee{...}), VS Code : dotnet tool install --global dotnet-ef, dotnet ef migrations add SeedEmployeesTable, dotnet ef migrations list, dotnet ef migrations remove, dotnet ef database update, Nuget Console : add-migration name, update-database

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -7,5 +7,18 @@ namespace EmployeeManagement.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Employee> Employees {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 3,
+                    Name = "Mark",
+                    Department = Dept.IT,
+                    Email = "mark@pragimtech.com"
+                }
+            );
+        }
     }
 }
