f9ef28a0d00894e2b5357e3b07024c3e3cb30ff9
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-14 23:57:31 +0530

    #56 Refactor Extract Method, ProcessUploadedFile

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -90,23 +90,17 @@ namespace EmployeeManagement.Controllers
          [HttpPost]
         public IActionResult Edit(EmployeeEditViewModel model)
         {
-            if(ModelState.IsValid) 
+            if(ModelState.IsValid)
             {
                 Employee employee = _employeeRepository.GetEmployee(model.Id);
                 employee.Name = model.Name;
                 employee.Email = model.Email;
                 employee.Department = model.Department;
+                
+                string uniqueFileName = ProcessUploadedFile(model);
 
-                string uniqueFileName = null;
-                if(model.Photo != null)
+                Employee newEmployee = new Employee
                 {
-                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
-                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                }
-
-                Employee newEmployee = new Employee{
                     Name = model.Name,
                     Email = model.Email,
                     Department = model.Department,
@@ -114,11 +108,24 @@ namespace EmployeeManagement.Controllers
                 };
 
                 _employeeRepository.Add(newEmployee);
-                return RedirectToAction("details", new {id=newEmployee.Id});
+                return RedirectToAction("details", new { id = newEmployee.Id });
             }
 
             return View();
         }
 
+        private string ProcessUploadedFile(EmployeeEditViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.Photo != null)
+            {
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
+            return uniqueFileName;
+        }
     }
 }
