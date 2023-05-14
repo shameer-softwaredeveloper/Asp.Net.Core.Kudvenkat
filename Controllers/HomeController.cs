2c8777c83f588454739e127eb5778d82c826bf3f
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-14 23:52:56 +0530

    #56 HttpPost Edit, EmployeeEditViewModel, model.Id Edit.cshtml <input hidden asp-for='Id'/> <input hidden asp-for='ExistingPhotoPath'/>

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -86,5 +86,39 @@ namespace EmployeeManagement.Controllers
 
             return View(employeeEditViewModel);
         }
+
+         [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if(ModelState.IsValid) 
+            {
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+
+                string uniqueFileName = null;
+                if(model.Photo != null)
+                {
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Employee newEmployee = new Employee{
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.Add(newEmployee);
+                return RedirectToAction("details", new {id=newEmployee.Id});
+            }
+
+            return View();
+        }
+
     }
 }
