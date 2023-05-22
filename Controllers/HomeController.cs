599649703952e66df9acb55061464e85025c12e9
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-23 01:28:35 +0530

    #64 LogLevel, using Microsoft.Extensions.Logging; public enum LogLevel {Trace = 0, Debug = 1, Information = 2, Warning = 3, Error = 4, Critical = 5, None = 6 } , appsettings.json, Default : Trace -- Log Trace Debug Info Warning Error Critical, Microsoft : Warning -- Log Warning Error Critical

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Controllers
 {
@@ -11,11 +12,14 @@ namespace EmployeeManagement.Controllers
     {
         private readonly IEmployeeRepository _employeeRepository;
         private readonly IHostingEnvironment hostingEnvironment;
+        private readonly ILogger logger;
         public HomeController(IEmployeeRepository employeeRepository, 
-                            IHostingEnvironment hostingEnvironment)
+                            IHostingEnvironment hostingEnvironment,
+                            ILogger<HomeController> logger)
         {
             _employeeRepository = employeeRepository;
             this.hostingEnvironment = hostingEnvironment;
+            this.logger = logger;
         }
 
         public ViewResult Index()
@@ -26,7 +30,14 @@ namespace EmployeeManagement.Controllers
 
         public ViewResult Details(int? id)
         {
-            throw new Exception("Error in Details View");
+            //throw new Exception("Error in Details View");
+
+            logger.LogTrace("Trace Log");
+            logger.LogDebug("Debug Log");
+            logger.LogInformation("Information Log");
+            logger.LogWarning("Warning Log");
+            logger.LogError("Error Log");
+            logger.LogCritical("Critical Log");
             
             Employee employee = _employeeRepository.GetEmployee(id.Value);
 
