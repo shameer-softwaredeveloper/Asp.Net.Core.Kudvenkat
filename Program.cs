381f74f7755a7eedfb47b013bc7cae30d9505adc
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-23 01:00:21 +0530

    #63 Logging to File using NLog, Nlog.Web.AspNetCore Nugget Package, using Microsoft.Extensions.Logging; using NLog.Extensions.Logging; nlog.config, .ConfigureLogging((hostingContext, loggingBuilder) =>,  loggingBuilder.AddNLog();

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -12,6 +14,21 @@ namespace EmployeeManagement
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, loggingBuilder) =>
+                {
+                    loggingBuilder.Configure(options =>
+                    {
+                        options.ActivityTrackingOptions = ActivityTrackingOptions.SpanId
+                                                            | ActivityTrackingOptions.TraceId
+                                                            | ActivityTrackingOptions.ParentId;
+                    });
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    loggingBuilder.AddConsole();
+                    loggingBuilder.AddDebug();
+                    loggingBuilder.AddEventSourceLogger();
+                    // Enable NLog as one of the Logging Provider
+                    loggingBuilder.AddNLog();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
