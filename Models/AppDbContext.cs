57f034bf94a3a72127c193b1362f899f4581a6a7
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-23 23:59:32 +0530

    #65 Identity, public class AppDbContext : IdentityDbContext , using Microsoft.AspNetCore.Identity.EntityFrameworkCore; Nugget Package ---> Microsoft.AspNetCore.Identity.EntityFrameworkCore

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace EmployeeManagement.Models
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
