139db833977806d14c7380807c5c495a0d3b9ec9
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-25 01:01:34 +0530

    #66 AccountController Register, [HttpGet] <form method='post'> label input span asp-asp-validation-summary asp-for asp-validation-for class='form-group' class='form-control' asp-controller asp-action class='navbar-nav ml-auto'

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
