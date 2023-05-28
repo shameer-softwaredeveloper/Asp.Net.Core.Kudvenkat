2c1d4d555e393b0768fffd8cdadb8e888bf36316
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-05-28 23:59:07 +0530

    #70 Login, System.ComponentModel.DataAnnotations; [Required] [EmailAddress] [DataType(DataType.Password)] [Display(Name = 'Remember me')], SignInResult = signInManager.PasswordSignInAsync(), bool IsPersistent -- RememberMe, SignInResult.Succeeded

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -44,6 +44,29 @@ namespace EmployeeManagement.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+            return View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
