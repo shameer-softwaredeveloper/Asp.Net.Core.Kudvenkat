bb11bed0a1ee59ff512a5ff773e6b3eaf22674a7
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-06-06 22:26:05 +0530

    #77 Extend IdentityUser, using Microsoft.AspNetCore.Identity; public class ApplicationUser : IdentityUser

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace EmployeeManagement.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace EmployeeManagement
         {
             services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDbConnection")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>(options => {
+            services.AddIdentity<ApplicationUser, IdentityRole>(options => {
                 options.Password.RequiredLength = 10;
                 options.Password.RequiredUniqueChars = 3;
                 options.Password.RequireNonAlphanumeric = false;
