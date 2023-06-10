eb9ef9db749dec542c16504cdb57b06f3e6770f0
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-06-10 23:37:31 +0530

    #81 <input type='hidden' so that when form is submit & post, values added to model reaches action method. While the fields remain hidden to the user

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -140,5 +140,47 @@ namespace EmployeeManagement.Controllers
 
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            var role = await rolemanager.FindByIdAsync(roleId);
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+            
+            for(int i = 0; i < model.Count; i++)
+            {
+                var user = await userManager.FindByIdAsync(model[i].UserId);
+
+                IdentityResult result = null;
+
+                if(model[i].IsSelected && !(await userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if(!(model[i].IsSelected) && await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if(result.Succeeded)
+                {
+                    if(i < (model.Count-1))
+                        continue;
+                    else
+                        return RedirectToAction("EditRole", new {Id = roleId});
+                }
+            }
+
+            return RedirectToAction("EditRole", new {Id = roleId});
+        }
     }
 }
