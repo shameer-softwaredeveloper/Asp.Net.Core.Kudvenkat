fe82e7257765ff61746e808aca9f1f83d3651b4a
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-06-11 03:51:44 +0530

    #83 Show or hide navigation menu 'Manage Role' based on user role, Access Denied, http://localhost:5000/Account/AccessDenied?ReturnUrl=%2Fadministration%2Flistroles

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -103,5 +103,12 @@ namespace EmployeeManagement.Controllers
             await signInManager.SignOutAsync();
             return RedirectToAction("index", "home");
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
