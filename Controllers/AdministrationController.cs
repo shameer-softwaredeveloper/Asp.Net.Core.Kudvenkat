13f2bff3e4b801a1ada2a085267a3066f4ed28b3
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-06-14 23:47:10 +0530

    #91 userManager.FindByIdAsync(userId); userManager.GetRolesAsync(user); userManager.RemoveFromRolesAsync(user, roles); userManager.AddToRolesAsync(user, model.Where(x => x.IsSelected).Select(y => y.RoleName)); ManageUserRoles(List<UserRolesViewModel> model, string userId) model from <form method='post' input 'submit' & userId from http://localhost:5000/Administration/ManageUserRoles?userId=24908c0a-0e0f-49d4-a5d1-685b5d683b2a, return RedirectToAction('EditUser', new {Id = userId});

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -62,6 +62,37 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpPost]
+        public async Task<IActionResult> ManageUserRoles(List<UserRolesViewModel> model, string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {userId} cannot be found";
+                return View("NotFound");
+            }
+
+            var roles = await userManager.GetRolesAsync(user);
+            var result = await userManager.RemoveFromRolesAsync(user, roles);
+
+            if(!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot remove user existing roles");
+                return View(model); 
+            }
+
+            result = await userManager.AddToRolesAsync(user, model.Where(x => x.IsSelected).Select(y => y.RoleName));
+
+            if(!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected roles to user");
+                return View(model);
+            }
+
+            return RedirectToAction("EditUser", new {Id = userId});
+        }
+
+        [HttpPost]
         public async Task<IActionResult> DeleteUser(string id)
         {
             var user = await userManager.FindByIdAsync(id);
