2f6e67b41026aec2bf2c22d90847b2466dde526f
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-06-16 02:55:44 +0530

    #95 Role based authorization vs claims based authorization, options.AddPolicy('AdminRolePolicy', policy => policy.RequireRole('Admin')); [Authorize(Policy = 'AdminRolePolicy')] --->  Role is a Claim with Type Role, that's how we are able to use role in combination with policy like we do for Claims. Role based authorization is old. Eg: Employees - Manager, Developer, Architect roles access link. Claim based authorization is new and recommended. Eg: Maternity leave only for Gender Claim Female, Claims from external identity provider like fb google etc. Multiple roles in policy ---> policy => policy.RequireRole('Admin', 'User', 'Manager')); Multiple claims in policy ---> policy => policy.RequireClaim('Delete Role').RequireClaim('Create Role')

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    [Authorize(Policy = "AdminRolePolicy")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> rolemanager;
--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,8 @@ namespace EmployeeManagement
             services.AddAuthorization(options => 
             {
                 options.AddPolicy("DeleteRolePolicy", policy => policy.RequireClaim("Delete Role").RequireClaim("Create Role"));
+
+                options.AddPolicy("AdminRolePolicy", policy => policy.RequireRole("Admin"));
             });
 
             //services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
