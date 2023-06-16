4bdb102663d7f91f54ce4fe4b20237d4ccc7d369
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-06-17 03:50:54 +0530

    #96 Claims based Authorization check in Views, options.AddPolicy('EditRolePolicy', policy => policy.RequireClaim('Edit Role'));  @using Microsoft.AspNetCore.Authorization  @inject IAuthorizationService authorizationService  @if((await authorizationService.AuthorizeAsync(User, 'EditRolePolicy')).Succeeded)  (awaitable, extension) Task<AuthorizationResult> IAuthorizationService.AuthorizeAsync(System.Security.Claims.ClaimsPrincipal user, string policyName)  System.Security.Claims.ClaimsPrincipal Microsoft.AspNetCore.Mvc.Razor.RazorPageBase.User { get; }

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,9 @@ namespace EmployeeManagement
 
             services.AddAuthorization(options => 
             {
-                options.AddPolicy("DeleteRolePolicy", policy => policy.RequireClaim("Delete Role").RequireClaim("Create Role"));
+                options.AddPolicy("DeleteRolePolicy", policy => policy.RequireClaim("Delete Role"));
+
+                options.AddPolicy("EditRolePolicy", policy => policy.RequireClaim("Edit Role"));
 
                 options.AddPolicy("AdminRolePolicy", policy => policy.RequireRole("Admin"));
             });
