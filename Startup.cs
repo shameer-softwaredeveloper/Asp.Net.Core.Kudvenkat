5c8fd06c7e36967485be8acae6ec6a6e10ca8921
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-06-23 00:01:05 +0530

    103 options.InvokeHandlersAfterFailure = false;  Default is true. Set false If you do not want the rest of the handlers to be called, when a failure is returned.

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,8 @@ namespace EmployeeManagement
                 //options.AddPolicy("EditRolePolicy", policy => policy.RequireAssertion(context => AuthorizeAccess(context)));
                 options.AddPolicy("EditRolePolicy", policy => policy.AddRequirements(new ManageAdminRolesAndClaimsRequirement()));
 
+                options.InvokeHandlersAfterFailure = false;
+                
                 options.AddPolicy("AdminRolePolicy", policy => policy.RequireRole("Admin"));
             });
 
