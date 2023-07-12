1910c9e0cd587d53be37b49dec75be92854521b8
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-07-13 00:41:39 +0530

    #121 Change Password, var result = await userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword); await signInManager.RefreshSignInAsync(user); var user = await userManager.GetUserAsync(User); public abstract class ControllerBase public ClaimsPrincipal User { get; }  Gets the System.Security.Claims.ClaimsPrincipal for user associated with the executing action. using System.ComponentModel.DataAnnotations; [Required] [DataType(DataType.Password)] [Display(Name = 'Confirm New Passwor')] [Compare('NewPassword', ErrorMessage = 'The New Password and Confirm New Password donot match')] NewPassword ---> Property.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -24,6 +24,42 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+                if(user == null)
+                {
+                    return RedirectToAction("Login");
+                }
+
+                var result = await userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+
+                if(!result.Succeeded)
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    return View();
+                }
+
+                await signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
         [AllowAnonymous]
         public IActionResult ResetPassword(string token, string email)
         {
