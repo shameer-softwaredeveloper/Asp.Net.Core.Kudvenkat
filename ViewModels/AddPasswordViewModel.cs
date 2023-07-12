f50796c28194699c09945798a2bb298a8af30f76
yourname@example.com <124866704+shameer-softwaredeveloper@users.noreply.github.com>
2023-07-13 02:05:37 +0530

    #122 Add password to local account linked to external login, var user = await userManager.GetUserAsync(User); var userHasPassword = await userManager.HasPasswordAsync(user); var result = await userManager.AddPasswordAsync(user, model.NewPassword); await signInManager.RefreshSignInAsync(user);

---
+++ ViewModels/AddPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class AddPasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("NewPassword", ErrorMessage = "The New Password and Confirm Password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
